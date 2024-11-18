203c6f7d2956af894d9f34b8375425f988b61dd9
terminaate <tntwnik@gmail.com>
2024-11-18 22:46:43 +0300

    initial commit, first version of updater

---
+++ src/main.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { check } from '@tauri-apps/plugin-updater';
+import { relaunch } from '@tauri-apps/plugin-process';
+
+const checkForUpdate = async () => {
+    const update = await check();
+    if (update) {
+        console.log(
+            `found update ${update.version} from ${update.date} with notes ${update.body}`
+        );
+        let downloaded = 0;
+        let contentLength = 0;
+        // alternatively we could also call update.download() and update.install() separately
+        await update.downloadAndInstall((event) => {
+            switch (event.event) {
+                case 'Started':
+                    contentLength = event.data.contentLength!;
+                    console.log(`started downloading ${event.data.contentLength} bytes`);
+                    break;
+                case 'Progress':
+                    downloaded += event.data.chunkLength;
+                    console.log(`downloaded ${downloaded} from ${contentLength}`);
+                    break;
+                case 'Finished':
+                    console.log('download finished');
+                    break;
+            }
+        });
+
+        console.log('update installed');
+        await relaunch();
+    }
+}
+
+const renderApp = () => {
+    ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+            <App />
+    );
+}
+
+const bootstrap = () => {
+    checkForUpdate().catch((er) => {
+        console.log(er)
+    })
+
+    renderApp();
+}
+
+void bootstrap();
